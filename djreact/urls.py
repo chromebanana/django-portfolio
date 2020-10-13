847bb8193d87f1be1a49919c1197633409de7ea3
William Cragg <wstc@riseup.net>
2020-10-13 15:07:22 +0100

    change to one server set up

--- djreact/urls.py
+++ djreact/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
+    path('', index, name='index'),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('articles.api.urls')),
-    re_path('.*', TemplateView.as_view(template_name='index.html'))
+
 
 ]
